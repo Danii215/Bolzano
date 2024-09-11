78f7e55519b3d1e8fe5abe42d59825f97f1aa0b5
Danilo M <danilosm215@gmail.com>
2024-09-11 02:42:09 -0300

    feat: Adição de metadados e ícones.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,41 @@
 import type { Metadata } from "next";
 import "@/assets/sass/main.scss";
 
-export const metadata: Metadata = {
-    title: "Bolzano",
-    description: "Projeto de Cálculo Numérico Computacional.",
-};
+export async function generateMetadata(): Promise<Metadata> {
+    return {
+        title: "Bolzano",
+        openGraph: {
+            locale: "pt_BR",
+            siteName: "Bolzano",
+        },
+        authors: [
+            {
+                name: "Danilo Movio",
+            },
+        ],
+        robots: {
+            index: true,
+            follow: true,
+            "max-image-preview": "large",
+            "max-snippet": 1,
+            "max-video-preview": 1,
+        },
+        icons: {
+            other: [
+                { rel: "icon", sizes: "16x16", url: "/favicon-16x16.png" },
+                { rel: "icon", sizes: "32x32", url: "/favicon-32x32.png" },
+                {
+                    rel: "apple-touch-icon",
+                    sizes: "100x100",
+                    url: "/apple-touch-icon.png",
+                },
+                { rel: "manifest", url: "/site.webmanifest" },
+                { rel: "mask-icon", url: "/safari-pinned-tab.svg" },
+            ],
+        },
+        manifest: "/site.webmanifest",
+    };
+}
 
 export default function RootLayout({
     children,
